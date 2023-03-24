51c3a4227eea520d03eb830c4ec5f11292d54765
ahn-sujin <asj9674@gmail.com>
2023-03-25 00:05:18 +0900

    Add:TouchableOpacity onpress 에 따른 버튼 활성화, 비활성화 효과 구현

--- App.js
+++ App.js
@@ -1,18 +1,56 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import { theme } from "./colors";
 
 export default function App() {
+  const [isActive, setIsActive] = useState("work");
+  const [text, setText] = useState("");
+  const handelActiveMenu = (menu) => {
+    setIsActive(menu);
+  };
+  const onChanageText = (payload) => setText(payload);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.tabMenu}>Work</Text>
+        <TouchableOpacity onPress={() => handelActiveMenu("work")}>
+          <Text
+            style={{
+              ...styles.tabMenu,
+              color: isActive === "work" ? theme.white : theme.gray,
+            }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.tabMenu}>Travel</Text>
+        <TouchableOpacity onPress={() => handelActiveMenu("travel")}>
+          <Text
+            style={{
+              ...styles.tabMenu,
+              color: isActive === "travel" ? theme.white : theme.gray,
+            }}
+          >
+            Travel
+          </Text>
         </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          onChangeText={onChanageText}
+          value={text}
+          placeholder={
+            isActive === "work" ? "Add a To Do" : "Where do you want to go?"
+          }
+          style={styles.input}
+        />
       </View>
     </View>
   );
@@ -30,8 +68,15 @@ const styles = StyleSheet.create({
     marginTop: 100,
   },
   tabMenu: {
-    color: theme.white,
     fontSize: 36,
     fontWeight: 700,
+  },
+  input: {
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    marginTop: 30,
+    backgroundColor: theme.white,
+    fontSize: 16,
+    borderRadius: 30,
   },
 });
